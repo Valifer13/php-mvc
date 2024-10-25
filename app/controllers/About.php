aa1a6fe73152b7d871f8b0c0c3ccb372c6acb889
Valifer13 <vianviszello@gmail.com>
2024-10-25 15:38:55 +0800

    Membuat views untuk controller Home dan About | chapter #5 View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,21 @@
 <?php 
 
-class About {
-    public function index($nama = "Guest", $pekerjaan = "Jobless") {
-        echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller {
+    public function index($nama = "Guest", $pekerjaan = "Jobless", $umur = 0) {
+        $data['nama']     = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur']     = $umur;
+        $data['judul'] = 'About Me';
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page() {
-        echo "Hello from about/page";
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+    <h1>Halaman About Me</h1>
+    <p>Halo, nama saya <?= $data['nama'] ?> dan pekerjaan saya adalah <?= $data['pekerjaan'] ?>. Saya berumur <?= $data['umur'] ?> tahun btw</p>
