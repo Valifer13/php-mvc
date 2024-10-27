5c70954d45f4b5025378a751b7509a070750ecd0
Valifer13 <vianviszello@gmail.com>
2024-10-27 16:05:10 +0800

    Memasang bootstrap dan menggunakan komponennya | chapter #6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,7 @@
-    <h1>Halaman About Me</h1>
+<div class="container d-grid justify-content-center mt-4 gap-3">
+    <h1 class="text-center">About Me</h1>
+    <div class="d-flex justify-content-center">
+        <img src="<?= BASE_URL; ?>/img/photo_profile.jpg" alt="Stelle" width="200" class="rounded-circle shadow">
+    </div>
     <p>Halo, nama saya <?= $data['nama'] ?> dan pekerjaan saya adalah <?= $data['pekerjaan'] ?>. Saya berumur <?= $data['umur'] ?> tahun btw</p>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,3 @@
+<div class="container d-flex justify-content-center mt-4">
     <h1>Selamat Datang di website saya!</h1>
+</div>
